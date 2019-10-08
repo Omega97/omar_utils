ca65ea3bff30d2cb08fdc69bcabeee6cba8aaa32
Omar Cusma Fait <31404553+Omega97@users.noreply.github.com>
2019-10-08 10:23:26 +0200

    Timer() returns self when called, added __repr__

--- tests/timer.py
+++ tests/timer.py
@@ -1,35 +1,52 @@
 """ Timer
 
 - initialize a timer
-- call it to obtain time passed from last call
-
+- call it to take time passed from last call
+- use .get_time to get time lapsed
 """
 from time import time
 
 
 class Timer:
     """timer"""
-    def __init__(self, threshold=-1.):
+    def __init__(self, tag='', precision=3):
         self.t = time()
-        self.threshold = threshold
+        self.lapse = None
+        self.precision = precision
+        self.tag = tag
 
-    def __call__(self, print_='', show=True, precision=3):
-        """call to take time from last call"""
+    def __repr__(self):
+        return f"[  {self.get_time():.{str(self.precision)}f} s  ] \t {self.tag}"
+
+    def take_time(self):
+        """takes time and computes lapse"""
         t = time()
-        delta = t - self.t
+        self.lapse = t - self.t
         self.t = t
-        if show:
-            if delta > self.threshold:
-                print(round(delta, precision), 's\t\t', print_)
-        return delta
+
+    def get_time(self):
+        """:returns time lapsed from last call"""
+        return self.lapse
+
+    def __call__(self, tag=None):
+        """call to take time from last call"""
+        if tag is not None:
+            self.tag = str(tag)
+        self.take_time()
+        return self
 
 
 if __name__ == '__main__':
 
     from time import sleep
 
-    timer = Timer()
+    timer = Timer(precision=3)
 
-    for I in range(-5, 0):
-        sleep(10**I)
-        timer()
+    for I in range(-11, -1):
+        # do stuff...
+        sleep(2**I)
+        # get time & print
+        print(timer(tag=I))
+        # use time (lapsed from last call)
+        if timer.get_time() > .1:
+            print('slow!')
