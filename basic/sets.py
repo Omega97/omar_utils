68cb83280d223cc688d6408efb2b573dcf73f355
Omar Cusma Fait <31404553+Omega97@users.noreply.github.com>
2019-10-08 08:28:40 +0200

    now Set(1) + 1 = Set(2)

--- basic/sets.py
+++ basic/sets.py
@@ -1,15 +1,17 @@
 """     Intuitive Set class
 
-- init a set using Set(a, b, c) or Set(*[a, b, c]) (to indicate that the array contains the arguments)
+- built on builtin sets (fast!), so it behaves in a similar manner
+- init a set using Set(a, b, c)
 - easy to sum/subtract/multiply sets
 - when summing remember to start from Set() !
 - a Set automatically searches for double elements, if you are in a hurry then Set(..., fast=True) skips search
 - use binary operations over two sets: & (AND), ^ (XOR), | (OR)
 - iter over a Set
 - also __eq__, __contains__
+- cannot create a set of list, sets or unhashable elements
 """
 __author__ = "Omar Cusma Fait"
-__version__ = "1.0.2"
+__version__ = "1.0.3"
 
 from copy import deepcopy
 
@@ -17,10 +19,10 @@ from copy import deepcopy
 class Set:
     """ Intuitive Set class """
     def __init__(self, *args):
-        """
-        :param args: elements of the Set, if you want to use a list of args, then do Set(*[...])
-        """
-        self.elements = {i for i in args}
+        try:
+            self.elements = {i for i in args}
+        except TypeError:
+            self.elements = {i for i in args[0]}
 
     def __call__(self, *args, **kwargs):
         return deepcopy(self.elements)
@@ -39,8 +41,13 @@ class Set:
 
     def __add__(self, other):
         if type(other) == Set:
+            # join sets
             return Set(*self(), *other())
+        elif type(other) == int or type(other) == float:
+            # apply number to set
+            return Set([i + other for i in self])
         else:
+            # add into set
             return self + Set(other)
 
     def __radd__(self, other):
@@ -48,13 +55,18 @@ class Set:
 
     def __sub__(self, other):
         if type(other) == Set:
+            # subtract set from set
             new = self()
             new.difference_update(other())
-            return Set(*new)
+            return Set(new)
+        elif type(other) == int or type(other) == float:
+            # apply number to set
+            return Set([i - other for i in self])
         else:
+            # subtract element from set
             new = self()
             new.difference_update(other)
-            return Set(*new)
+            return Set(new)
 
     def __mul__(self, other):
         if type(other) == Set:
@@ -62,13 +74,16 @@ class Set:
             for i in self:
                 for j in other:
                     v += [i * j]
-            return Set(*v)
+            return Set(v)
+        elif type(other) == int or type(other) == float:
+            # apply number to set
+            return Set([i * other for i in self])
 
     def __contains__(self, item):
         return True if item in self.elements else False
 
     def intersect(self, other):
-        return Set(*[i for i in self if i in other])
+        return Set([i for i in self if i in other])
 
     def __eq__(self, other):
         return self() == other()
@@ -84,19 +99,21 @@ class Set:
 
     def apply(self, fun):
         """apply fun on each element of the set"""
-        return Set(*[fun(i) for i in self])
+        return Set([fun(i) for i in self])
 
 
 if __name__ == '__main__':
 
-    assert Set() + 'e' == Set('e')
-    assert 'e' + Set() == Set('e')
-    assert Set('e') - 'e' == Set()
+    assert Set('a') + Set('b') == Set('a', 'b')
+    assert Set('a', 'b') - Set('b') == Set('a')
     assert Set(1, 2) + Set(2, 3) == Set(1, 2, 3)
     assert Set(1, 2) - Set(2, 3) == Set(1)
-    assert Set('a', 'b') * Set(3, 2) == Set(*['aa', 'aaa', 'bb', 'bbb'])
-    assert Set(1, 2, 3) | Set(3, 4, 5) == Set(*[i+1 for i in range(5)])
+    assert Set(1, 2) + 1 == Set(2, 3)
+    assert Set(1, 2) - 1 == Set(0, 1)
+    assert Set(1, 2) * 2 == Set(2, 4)
+    assert Set('a', 'b') * Set(3, 2) == Set(['aa', 'aaa', 'bb', 'bbb'])
+    assert Set(1, 2, 3) | Set(3, 4, 5) == Set([i+1 for i in range(5)])
     assert Set(1, 2, 3) & Set(3, 4, 5) == Set(3)
     assert Set(1, 2, 3) ^ Set(3, 4, 5) == Set(1, 2, 4, 5)
     assert Set(1, 2, 3).apply(lambda x: x+1) == Set(2, 3, 4)
-    assert sum([Set(i+1) for i in range(10)], Set()) == Set(*[i+1 for i in range(10)])
+    assert sum([Set(i+1) for i in range(5)]) == Set([i+1 for i in range(5)])
