1dbc9cec6bf3d3fbcd22170a3b6b0b8563977fbd
Omar Cusma Fait <31404553+Omega97@users.noreply.github.com>
2020-03-04 12:54:25 +0100

    import files lib

--- __init__.py
+++ __init__.py
@@ -1,10 +1,17 @@
 """         omar_utils
 
 Provides common and useful methods of all kinds
+Files are not supposed to depend on one another since they may be frequently updated
 """
-from omar_utils.basic.iter_utils import *
+from omar_utils.basic import iter_utils
+from omar_utils.basic import files
 
 
 __author__ = "Omar Cusma Fait"
-__date__ = (18, 12, 2019)
-__version__ = "1.2.0"
+__date__ = (26, 2, 2020)
+__version__ = "1.3.0"
+
+
+if __name__ == '__main__':
+    print('iter_utils', iter_utils.__version__)
+    print('files', files.__version__)
