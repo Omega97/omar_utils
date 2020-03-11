7fbc4dc1be0b4e5f645feaa5e6399f2ea23f74df
Omar Cusma Fait <31404553+Omega97@users.noreply.github.com>
2020-03-11 20:11:15 +0100

    added degub decorator

--- basic/decorators.py
+++ basic/decorators.py
@@ -1,3 +1,4 @@
+__date__ = (8, 3, 2020)     # debug
 from time import time
 
 
@@ -20,6 +21,46 @@ def take_average_time(n):
             return (time() - t) / n
         return wrapper2
     return wrapper1
+
+
+def debug(fun):
+    def wrap(*args, **kwargs):
+        s = f'{fun.__name__}('
+        if len(args):
+            s += f'{", ".join([str(i) for i in args])}'
+        if len(kwargs):
+            s += f', {", ".join([str(i) + "=" + str(kwargs[i]) for i in kwargs])}'
+        s += ')'
+        print(s, '...')
+        out = fun(*args, **kwargs)
+        s += f' -> {out}'
+        print(s)
+        return out
+    return wrap
+
+
+class Debug:
+
+    count = 0
+
+    def __call__(self, fun):
+        def wrap(*args, **kwargs):
+            s = f'{fun.__name__}('
+            if len(args):
+                s += f'{", ".join([str(i) for i in args])}'
+            if len(kwargs):
+                s += f', {", ".join([str(i) + "=" + str(kwargs[i]) for i in kwargs])}'
+            s += ')'
+            print('|\t' * (Debug.count))
+            print('|\t' * Debug.count, s)
+            Debug.count += 1
+            out = fun(*args, **kwargs)
+            s = f'{out}'
+            Debug.count -= 1
+            print('|\t' * (Debug.count + 1))
+            print('|\t' * Debug.count, s)
+            return out
+        return wrap
 
 
 if __name__ == '__main__':
