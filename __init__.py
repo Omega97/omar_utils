92161e1822e2d277b4ab6e598eb716d38386f059
Omar Cusma Fait <31404553+Omega97@users.noreply.github.com>
2020-03-11 20:05:02 +0100

    added more imports

--- __init__.py
+++ __init__.py
@@ -4,14 +4,18 @@ Provides common and useful methods of all kinds
 Files are not supposed to depend on one another since they may be frequently updated
 """
 from omar_utils.basic import iter_utils
+from omar_utils.basic.iter_utils import *
 from omar_utils.basic import files
+from omar_utils.basic.files import load_obj, save_obj
+from omar_utils.basic.decorators import Debug
 
 
 __author__ = "Omar Cusma Fait"
-__date__ = (3, 3, 2020)
+__date__ = (9, 3, 2020)
 __version__ = "1.3.1"
 
 
 if __name__ == '__main__':
     print('iter_utils', iter_utils.__version__)
     print('files', files.__version__)
+    print(q_print, load_obj, save_obj, Debug)
